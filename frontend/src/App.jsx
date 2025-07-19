c1702209a7fbe0f866d721dae0e03790b49dff3e
Raymond Chen <dwuh236@gmail.com>
2025-07-19 16:14:23 -0400

    set up elementary routing to some pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Routes, Route} from 'react-router-dom'  
 import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+
 
 
 function App() {
 
   return (
-    <>
-    <Home />
-    </>
+    <main className = "main-content">
+        <Routes>
+            <Route path = "/" element = {<Home />} />
+            <Route path = "/favorites" element = {<Favorites />} />
+        </Routes>
+    </main>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,34 @@
 import MovieCard from "../components/MovieCard"
+import {useState} from "react"
+
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
-        {id : 1, title: " movieszzz", release_date: "2024"},
-        {id : 2, title: " movieszzz", release_date: "2024"},
-        {id : 3, title: " movieszzz", release_date: "2024"},
-        {id : 4, title: " movieszzz", release_date: "2024"},
+        {id : 1, title: "terminator", release_date: "2021"},
+        {id : 2, title: "john wick 1", release_date: "2022"},
+        {id : 3, title: "get out", release_date: "2023"},
+        {id : 4, title: "parasite", release_date: "2024"},
     ]
-
-    const handleSearch = () => {
-
-    }
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    };
     
     return (
         <div className = "home">
             <form onSubmit={handleSearch} className = "search-form">  
-                <input type = "text" placeholder="Search For Movies" className = "search-input"></input>
+                <input type = "text" placeholder="Search For Movies" className = "search-input" value = {searchQuery} onChange = {(e) => setSearchQuery(e.target.value)}></input>
+                <button type = "search" className = "search-button">Search</button>
             </form>
 
             <div className = "movies-grid">
                 {movies.map(movie => <MovieCard movie = {movie} key = {movie.id}/>)}
             </div>
         </div>
+        
     )
 }
 
