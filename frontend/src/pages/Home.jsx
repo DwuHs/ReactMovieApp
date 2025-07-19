1d316c9edb32e996e8e2d0d46b3772f9b2e059ed
Raymond Chen <dwuh236@gmail.com>
2025-07-19 17:37:50 -0400

    debugged api functions, added functions to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,33 @@
 import MovieCard from "../components/MovieCard"
-import {useState} from "react"
+import {useState, useEffect, use} from "react"
+import { getPopularMovies, searchMovies} from "../services/api"
 import '../css/Home.css'
 
 
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const movies = [
-        {id : 1, title: "terminator", release_date: "2021"},
-        {id : 2, title: "john wick 1", release_date: "2022"},
-        {id : 3, title: "get out", release_date: "2023"},
-        {id : 4, title: "parasite", release_date: "2024"},
-    ]
+    useEffect(() => {
+        const loadPopularMovies = async () =>{
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } 
+            catch(err){
+                console.log(err)
+                setError("Failed to load movies")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+
+        loadPopularMovies()
+    }, [])  
+
     const handleSearch = (e) => {
         e.preventDefault()
         alert(searchQuery)
@@ -26,7 +42,7 @@ function Home() {
             </form>
 
             <div className = "movies-grid">
-                {movies.map(movie => <MovieCard movie = {movie} key = {movie.id}/>)}
+                {movies.map((movie) => (<MovieCard movie= {movie} key={movie.id} />))}
             </div>
         </div>
         
